03cdfe0463d04914269bf75c0feebc0b77f52be6
ibadullahesen <ibadulla.hesen@gmail.com>
2026-02-20 11:50:25 +0300

    Create AdsPanel.tsx

---
+++ components/AdsPanel.tsx
@@ -0,0 +1,14 @@
+export default function AdsPanel() {
+  return (
+    <>
+      <div className="hidden lg:block fixed left-0 top-20 w-60 h-[80vh] glass p-4">
+        <h3>Burada sizin reklamınız ola bilər</h3>
+        <p>Reklam yerləşdirmək üçün bizimlə əlaqə saxlayın</p>
+      </div>
+      <div className="hidden lg:block fixed right-0 top-20 w-60 h-[80vh] glass p-4">
+        <h3>Burada sizin reklamınız ola bilər</h3>
+        <p>Reklam yerləşdirmək üçün bizimlə əlaqə saxlayın</p>
+      </div>
+    </>
+  )
+}
